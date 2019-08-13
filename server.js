6d5b5d97c17ec1ccc2d7879778d454872a101859
Katherine Lewis <kale8432@colorado.edu>
2019-08-13 07:46:47 -0400

    added server annotations

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
+//import express
 const app = express();
+//use express
 const environment = process.env.NODE_ENV || 'development';
+//set environement to the node environment or developement in migrations
 const configuration = require('./knexfile')[environment];
+//config the environment by using the knexfile environment
 const database = require('knex')(configuration);
+//use knex and the config aka knex environment
 const cors = require('cors');
+//import cors
 const port = process.env.PORT || 3000;
+//set the port to the port default env or 3000
 
 app.use(express.json());
+//use express in the app
 app.use(cors());
+//use cors
 app.set('port', port);
+//set the port
 
 app.listen(app.get('port'), () => {
 	console.log(`App is running on http://localhost/${app.get('port')}.`);
 });
+//listen for the port, get the port, and console log which port the app is running on
 
 app.get('/', (req, res) => {
 	res.status(200).json('TEST');
 });
+//get the root route and print 'TEST' when the request is fulfilled
 
 app.get('/api/v1/books', (req, res) => {
 	database('books')
@@ -28,6 +40,8 @@ app.get('/api/v1/books', (req, res) => {
 		})
 		.catch(error => res.status(500).json({ error }));
 });
+
+//get that data at this route (/api/v1/books) in the books database, select it, .then the promise or catch it. If the promise returns data => request fulfilled otherwise the data could not be found and a 404 is returned. If the promise fails, a 500 server error is returned.
 
 app.get('/api/v1/books/:id', (req, res) => {
 	database('books')
@@ -41,6 +55,8 @@ app.get('/api/v1/books/:id', (req, res) => {
 		.catch(error => res.status(500).json({ error }));
 });
 
+//get that data at this route (/api/v1/books/:id) in the books database, select it, .then the promise or catch it. If the promise returns data => request fulfilled otherwise the data could not be found and a 404 is returned. If the promise fails, a 500 server error is returned.
+
 app.get('/api/v1/reviews', (req, res) => {
 	database('reviews')
 		.select()
@@ -49,6 +65,8 @@ app.get('/api/v1/reviews', (req, res) => {
 		})
 		.catch(error => res.status(500).json({ error }));
 });
+
+//get that data at this route (/api/v1/reviews) in the reviews database, select it, .then the promise or catch it. If the promise returns data => request fulfilled otherwise the data could not be found and a 404 is returned. If the promise fails, a 500 server error is returned.
 
 app.get('/api/v1/reviews/:id', (req, res) => {
 	database('reviews')
@@ -59,6 +77,8 @@ app.get('/api/v1/reviews/:id', (req, res) => {
 		})
 		.catch(error => res.status(500).json({ error }));
 });
+
+//get that data at this route (/api/v1/reviews/:id) in the reviews database, select it, .then the promise or catch it. If the promise returns data => request fulfilled otherwise the data could not be found and a 404 is returned. If the promise fails, a 500 server error is returned.
 
 app.post('/api/v1/books', (req, res) => {
 	const book = req.body;
@@ -77,6 +97,8 @@ app.post('/api/v1/books', (req, res) => {
 		.catch(error => res.status(500).json({ error }));
 });
 
+//A post request is made to (/api/v1/books) the server. In order to make sure the post has everything the database needs, the required parameters are checked. If it does not contain the correct parameters, a 422 (inprocessable entity) is returned with a message of the required parameters and the missing parameter(s). The post is made to the books database, given an id, and either the promise responds with 201 with the post's new id to show that the entry has been created or catches with a 500 error, the server had an internal issue.
+
 app.post('/api/v1/reviews', (req, res) => {
 	const review = req.body;
 
@@ -94,6 +116,8 @@ app.post('/api/v1/reviews', (req, res) => {
 		.catch(error => res.status(500).json({ error }));
 });
 
+//A post request is made to (/api/v1/reviews) the server. In order to make sure the post has everything the database needs, the required parameters are checked. If it does not contain the correct parameters, a 422 (inprocessable entity) is returned with a message of the required parameters and the missing parameter(s). The post is made to the reviews database, given an id, and either the promise responds with 201 with the post's new id to show that the entry has been created or catches with a 500 error, the server had an internal issue.
+
 app.delete('/api/v1/:table/:id', (req, res) => {
 	const { table, id } = req.params;
 
@@ -108,3 +132,5 @@ app.delete('/api/v1/:table/:id', (req, res) => {
 		})
 		.catch(error => res.status(500).json({ error }));
 });
+
+//A request to delete data is made at this route (/api/v1/:table/:id). Depending on the requests' table and id, the server processes the request in the appropraite table and selects the id. It deletes the entry and then returns a promise with either a 404, no entry found for the id in the table, or a 200 successfully deleted message. Otherwise if the promise failed it catches with a 500 internal server error response message.
